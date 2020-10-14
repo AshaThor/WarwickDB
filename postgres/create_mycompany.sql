6a7a3c9c301c81ef976c5f7d605e59c94cd7f76d
AshaThor <smartyparty101@hotmail.com>
2020-10-14 10:52:18 +0100

    added postgres

--- postgres/create_mycompany.sql
+++ postgres/create_mycompany.sql
@@ -1,24 +1,26 @@
-CREATE DATABASE mycompany;
-USE mycompany;
-CREATE TABLE dept (deptno int, dname VARCHAR(20) NOT NULL, loc VARCHAR(20), PRIMARY KEY (deptno));
-/*Issues were lacking primary key and dept was a char*/
+CREATE TABLE dept (
+	deptno NUMERIC PRIMARY KEY, 
+	dname VARCHAR(20) NOT NULL, 
+	loc VARCHAR(20)
+);
 
 CREATE TABLE emp (
-empno INT (4) NOT NULL,
+empno serial NOT NULL,
 ename VARCHAR (20) NOT NULL,
 job VARCHAR (14) NOT NULL,
-mgr INT (4) NOT NULL,
+mgr NUMERIC(4) NOT NULL,
 hiredate DATE, 
 sal NUMERIC (7,2) NOT NULL,
 comm NUMERIC (7,2),
-deptno INT(2),
+deptno NUMERIC(2),
 PRIMARY KEY (empno),
 FOREIGN KEY (deptno) REFERENCES dept(deptno),
 CHECK (sal > 0),
-CHECK (comm >= 0));
+CHECK (comm >= 0)
+);
 
 CREATE TABLE salgrade (
-grade INT (1), 
+grade NUMERIC(1), 
 losal NUMERIC (7,2) NOT NULL, 
 hisal NUMERIC (7,2), 
 PRIMARY KEY (grade), 
@@ -27,7 +29,7 @@ CHECK (hisal >= losal)
 );
 
 CREATE TABLE customer (
-cno int NOT NULL AUTO_INCREMENT, 
+cno serial NOT NULL, 
 orderdate DATE, 
 name VARCHAR(15),
 city VARCHAR(15), 
@@ -35,12 +37,20 @@ country VARCHAR(15),
 pno NUMERIC(4), 
 description VARCHAR(20),
 qty NUMERIC(4) CHECK (qty >= 1), 
-PRIMARY KEY (cno));
+PRIMARY KEY (cno)
+);
 
-create table proj
-(
+create table proj(
 	projno serial PRIMARY KEY,
 	pname VARCHAR(32),
 	budget Numeric(8,2) NOT NULL check (budget >= 0)
 	
+);
+
+create table empproj_expenses(
+	emp integer references emp(empno),
+	projno integer references proj(projno),
+	claimdate DATE NOT NULL,
+	claimtotal NUMERIC(7,2),
+	PRIMARY KEY (emp, projno, claimdate)
 );
